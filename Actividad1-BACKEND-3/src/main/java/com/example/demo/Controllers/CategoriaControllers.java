86f7ea5a7fcf3a1c148065b04b26447971d1684b
adrianvz94 <105075239+adrianvz94@users.noreply.github.com>
2022-05-17 11:46:43 -0500

    Add files via upload

--- Actividad1-BACKEND-3/src/main/java/com/example/demo/Controllers/CategoriaControllers.java
+++ Actividad1-BACKEND-3/src/main/java/com/example/demo/Controllers/CategoriaControllers.java
@@ -1,13 +1,20 @@
 package com.example.demo.Controllers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.junit.*;
+
 
 import com.example.demo.model.Qacatego;
+
+import io.swagger.annotations.ApiOperation;
+
 import com.example.demo.Repository.CategoriaRepository;
 import com.example.demo.Services.ServicesCatego;
 
@@ -18,24 +25,33 @@ public class CategoriaControllers
 	@Autowired
 	private ServicesCatego cateservicio;
 	
+	@Test
+	public void ObtenerTest()
+	{
+		List<Qacatego> catego = cateservicio.getAllCategorias();
+		assertThat(catego).size().isGreaterThan(0);
+		System.out.println("Muetsra de Test");
+	}
+	
 	@GetMapping
+	@ApiOperation (value = "Mostrar informacion", notes= "Motrar Registros de la tabla Categoria")
 	public List<Qacatego> MostrarCategorias()
 	{
 		return cateservicio.getAllCategorias();
 	}
-	
+	@ApiOperation (value = "Agregar un Registro", notes= "Este metodo es utilizado para agregar un registro.")
 	@PostMapping("/Agregar")
 	public Qacatego Add (@RequestBody Qacatego categoria)
 	{
 		return this.cateservicio.Add(categoria);
 	}
-	
+	@ApiOperation (value = "Actualizar Registros", notes= "Actualizo los registros de la BD.")
 	@PutMapping("/Actualizar")
 	public Qacatego Update (@RequestBody Qacatego categoria)
 	{
 		return this.cateservicio.actualizar(categoria);
 	}
-	
+	@ApiOperation (value = "Elimina Registros", notes= "Eliminar registro de la base de datos.")
 	@DeleteMapping("/Eliminar")
 	public Qacatego Delete (@RequestBody Qacatego categoria)
 	{
--- Actividad1-BACKEND-3/src/main/java/com/example/demo/Controllers/ProductoControllers.java
+++ Actividad1-BACKEND-3/src/main/java/com/example/demo/Controllers/ProductoControllers.java
@@ -2,13 +2,18 @@ package com.example.demo.Controllers;
 
 import java.util.Optional;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.junit.*;
 
+import com.example.demo.model.Qacatego;
 import com.example.demo.model.Qaproduct;
 import com.example.demo.Services.ServicesProduct;
 
@@ -22,6 +27,15 @@ public class ProductoControllers
 	@Autowired
 	private ServicesProduct prodservicio;
 	
+	@Test
+	public void ObtenerTest()
+	{
+		List<Qaproduct> Product = prodservicio.getAllCategorias();
+		assertThat(Product).size().isGreaterThan(0);
+		System.out.println("Muetsra de Test");
+	}
+	
+
 	@GetMapping("/Mostrar")
 	public List<Qaproduct> MostrarCategorias()
 	{
